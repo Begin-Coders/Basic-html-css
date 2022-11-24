cd3491815abd4eb63da87752e525434fa33ff21b
GoAmeer030 <59198817+GoAmeer030@users.noreply.github.com>
2022-11-24 10:50:01 +0530

    added `tamzil and madan sites`

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ let buttons = {
     button0: document.getElementById('show-site-0'),
     button1: document.getElementById('show-site-1'),
     button2: document.getElementById('show-site-2'),
-    button3: document.getElementById('show-site-3')
+    button3: document.getElementById('show-site-3'),
+    button4: document.getElementById('show-site-4'),
+    button5: document.getElementById('show-site-5')
 }
 
 buttons.button0.onclick = () => document.getElementById('webpage-body-div').innerHTML = null
@@ -19,5 +21,15 @@ buttons.button2.onclick = () => {
 
 buttons.button3.onclick = () => {
     document.getElementById('webpage-body-div').innerHTML = `<iframe src='./dhinagar-login/index.html'></iframe><button onclick="location.href='./dhinagar-login/index.html'">Click to see in full screen</button>`
+    design()
+}
+
+buttons.button4.onclick = () => {
+    document.getElementById('webpage-body-div').innerHTML = `<iframe src='./madhan-login/index.html'></iframe><button onclick="location.href='./madan-login/index.html'">Click to see in full screen</button>`
+    design()
+}
+
+buttons.button5.onclick = () => {
+    document.getElementById('webpage-body-div').innerHTML = `<iframe src='./tamz-login/index.html'></iframe><button onclick="location.href='./tamz-login/index.html'">Click to see in full screen</button>`
     design()
 }
